94c368e3f480b04287681cbffa8904ad5e49b817
Ketzha Agni <agniwwe@gmail.com>
2025-07-21 09:41:54 +0000

    Revert "modified:   src/app/layout.tsx"
    This reverts commit 851d4cbeb42b21906f6bef0be60a7aca360fca13.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
-import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: 'TourEase - Perencana Perjalanan AI Anda',
@@ -30,7 +29,6 @@ export default function RootLayout({
       <body className="font-body antialiased">
         {children}
         <Toaster />
-        <Analytics />
       </body>
     </html>
   );
