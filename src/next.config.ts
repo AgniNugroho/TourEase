7f5a9129f521cbe6d8c9f97644a760c0a7a8ba1b
Ketzha Agni <agniwwe@gmail.com>
2025-07-21 11:18:54 +0000

    esalahan: Invalid src prop (https://lh5.googleusercontent.com/p/AF1QipN6

---
+++ src/next.config.ts
@@ -0,0 +1,42 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'maps.googleapis.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  allowedDevOrigins: [
+    '192.168.1.9'
+  ],
+  // Server Actions are enabled by default in Next.js 14+
+  // experimental: {
+  //   serverActions: true, // or { bodySizeLimit: '2mb' } for example
+  // },
+};
+
+export default nextConfig;
